c7e1e5f16b364f6057226a5005c7c5e6ed7eff1d
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-09 14:39:46 +0100

    wcięcia

--- 24_warunki.sh
+++ 24_warunki.sh
@@ -4,7 +4,12 @@ read -p "Podaj nazwę konta: " user
 
 if id "$user"
 then
-	echo "Użytkownik $user istnieje"
+    echo "Użytkownik $user istnieje"
+else
+    echo "Brak konta $user."
+    read -p "Czy chcesz je założyć? " czy_zalozyc
+    # Jakiś kod zadający pytanie i zakładający konto
+    echo "Odpowiedziałeś: $czy_zalozyc"
 fi
 
 echo KONIEC
