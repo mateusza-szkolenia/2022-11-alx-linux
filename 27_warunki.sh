f08fc2e1504f24de0d5aa660a9cd825c6350c9f7
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-09 15:56:41 +0100

    porównywanie liczb

---
+++ 27_warunki.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "podaj liczbę większą od 100: " liczba
+
+# gt    greater than    >
+# lt    less then       <
+# eq    equal           = 
+
+
+if test "$liczba" -gt 100
+then
+    echo "OK"
+else
+    echo "Wracaj do szkoły"
+    exit 999
+fi
