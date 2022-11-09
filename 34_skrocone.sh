b062268dd207034b81f5617c3eaaca2518f887bc
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-09 16:51:36 +0100

    przed

---
+++ 34_skrocone.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+biez="$(date +%Y)"
+
+read -p "Podaj rok urodzenia: " ur
+
+echo "Rok urodzenia: $ur"
+
+(( wiek = biez - ur ))
+
+echo "Wiek: $wiek"
+
+if (( wiek >= 18 ))
+then
+    echo "Pełnoletni"
+else
+    echo "Nieletni"
+fi
+
