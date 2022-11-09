8b558d1df290c9af9bb9392f4be0155e04272d59
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-09 13:39:39 +0100

    arytmetyka

---
+++ 20_arytmetyka.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+a=10
+b=20
+
+echo "a + b = $((a + b))"
+
+# tylko liczby całkowite
+echo "100 podzielić na 3: $(( 100 / 3 ))"
+echo "100 modulo 3: $(( 100 % 3 ))"
+
+razem=987
+zrobione=129
+
+echo "Postęp: $(( 100 * zrobione / razem ))%"
