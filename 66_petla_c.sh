b80854d1802eee7054b4255d3a2436b10bbf19c1
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-10 13:26:28 +0100

    pętla for w stylu C

---
+++ 66_petla_c.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# bardzo rzadko używana
+# wzorowana na języku C
+
+for ((i=0; i<30; i++))
+do
+    echo "liczba $i"
+
+    if (( i == 5 ))
+    then
+        (( i = 15 ))
+    fi
+
+done
