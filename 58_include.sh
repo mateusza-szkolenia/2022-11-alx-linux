3673af186f14b8e9d294983a3c96581cb4f4c4f6
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-10 11:35:45 +0100

    zmienne lokalne

---
+++ 58_include.sh
@@ -0,0 +1,10 @@
+
+cokolwiek () {
+    local imie
+    # dużo dużo kodu
+
+    
+    imie=XXX
+    echo "Witamy $imie"
+    # dużo dużo kodu
+}
---
+++ 58_przyklad.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source 58_include.sh
+
+imie=Mateusz
+
+echo "Nazywam się $imie"
+
+cokolwiek
+
+echo "Nazywam się cały czas $imie"
